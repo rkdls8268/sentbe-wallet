8514ca07407229b847c8058501c74228c408ed92
rkdls8268 <rkdls8268@gmail.com>
2026-03-08 22:51:19 +0900

    [FEAT] first commit

---
+++ src/main/java/com/sentbe/SentbeApplication.java
@@ -0,0 +1,13 @@
+package com.sentbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentbeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SentbeApplication.class, args);
+  }
+
+}
